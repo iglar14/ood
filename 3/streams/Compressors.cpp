49ecdcf295b5fc54d70055adb02082e604b2a137
Igor Larionov <igor.larionov@cpslabs.net>
2018-05-26 19:32:22 +0300

    Начал архивирование (3.3)

---
+++ 3/streams/Compressors.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+#include "Compressors.h"
+
+using namespace std;
+
+CRleCompress::CRleCompress(IOutputDataStream& stream)
+	: m_stream(stream)
+{
+
+}
+
+CRleCompress::~CRleCompress()
+{
+
+}
+
+void CRleCompress::WriteByte(uint8_t data)
+{
+
+}
+void CRleCompress::WriteBlock(const void * srcData, std::streamsize size)
+{
+
+}
+
+CRleDecompress::CRleDecompress(IInputDataStream& stream)
+	: m_stream(stream)
+{
+
+}
+
+CRleDecompress::~CRleDecompress()
+{
+
+}
+
+bool CRleDecompress::IsEOF()const
+{
+	return (m_counter == 0) && m_stream.IsEOF();
+}
+
+uint8_t CRleDecompress::ReadByte()
+{
+	if (m_counter < 0)
+	{
+		++m_counter;
+		return m_stream.ReadByte();
+	}
+	else if (m_counter > 0)
+	{
+		--m_counter;
+		return m_value;
+	}
+	else	// m_counter == 0
+	{
+		m_counter = m_stream.ReadByte();
+		m_value = m_stream.ReadByte();
+		if (m_counter == 0)
+		{
+			throw ios_base::failure("Broken compressed data format");
+		}
+		return ReadByte();
+	}
+}
+
+std::streamsize CRleDecompress::ReadBlock(void * dstBuffer, std::streamsize size)
+{
+
+}
---
+++ 3/streams/Compressors.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "IStreams.h"
+
+class CRleCompress final : public IOutputDataStream
+{
+public:
+	CRleCompress(IOutputDataStream& stream);
+	~CRleCompress() override;
+
+	void WriteByte(uint8_t data) override;
+	void WriteBlock(const void * srcData, std::streamsize size) override;
+private:
+	IOutputDataStream& m_stream;
+};
+
+class CRleDecompress final : public IInputDataStream
+{
+public:
+	CRleDecompress(IInputDataStream& stream);
+	~CRleDecompress() override;
+
+	bool IsEOF()const override;
+	uint8_t ReadByte() override;
+	std::streamsize ReadBlock(void * dstBuffer, std::streamsize size) override;
+private:
+	IInputDataStream& m_stream;
+	char m_counter = 0;
+	uint8_t m_value = 0;
+};
